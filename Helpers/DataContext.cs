25bc386b73a0e5f9f0ed85e31d08ae0b0ba32352
ilyes24 <i.ghomrani@esi-sba.dz>
2019-08-10 11:40:58 +0100

    setting up the DataContext

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -1,7 +1,12 @@
-﻿namespace UserManagement.Helpers
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Entities;
+
+namespace UserManagement.Helpers
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
     }
 }
