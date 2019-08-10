de008879a0c995f26b74c895fbf79250acfb7ce7
ilyes24 <i.ghomrani@esi-sba.dz>
2019-08-10 11:38:11 +0100

    setting up UserDto and EF AutoMapper

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UserManagement.Dtos;
+using UserManagement.Entities;
+
+namespace UserManagement.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserEntity, UserDto>();
+            CreateMap<UserDto, UserEntity>();
+        }
+    }
+}
